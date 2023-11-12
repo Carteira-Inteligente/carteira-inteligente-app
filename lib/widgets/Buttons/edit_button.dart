a40b0fe4666899929062f23b298611dce97da299
Thiago Proença <thiagoenca@gmail.com>
2023-11-11 23:30:19 -0300

    ajuste teste

--- lib/widgets/Buttons/edit_button.dart
+++ lib/widgets/Buttons/edit_button.dart
@@ -1,55 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:jt_eventos_flutter/constants/constants.dart';
+
+import '../../constants/colors.dart';
+import '../../constants/svgs.dart';
+import '../../constants/widgets.dart';
+import '../Containers/button_containers.dart';
 
 class EditButton extends StatelessWidget {
-  const EditButton(this.onPressed, {super.key});
+  const EditButton({
+    super.key,
+    required this.onPressed,
+  });
 
   final void Function() onPressed;
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Container(
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                colors: cEditButton,
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-              ),
-              borderRadius: BorderRadius.all(
-                Radius.circular(50),
-              ),
-            ),
-            child: ElevatedButton.icon(
-              icon: SvgPicture.asset(
-                sEdit,
-                color: cWhite,
-              ),
-              style: ElevatedButton.styleFrom(
-                  textStyle: const TextStyle(
-                    fontSize: 20,
-                  ),
-                  minimumSize: const Size(170, 60),
-                  shape: const RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(50),
-                    ),
-                  ),
-                  backgroundColor: cTransparent,
-                  elevation: 0),
-              onPressed: onPressed,
-              label: Text(
-                "Editar",
-                style: Theme.of(context).textTheme.headline5,
-              ),
-            ),
-          )
-        ],
+    return ButtonContainer(
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          minimumSize: wRoundButtonSize,
+          shape: wButtonBorderRadius,
+          side: BorderSide(color: cGrey.shade800),
+          backgroundColor: cWhite,
+          elevation: 0,
+        ),
+        onPressed: onPressed,
+        child: SvgPicture.asset(
+          sEdit,
+          color: cGrey.shade800,
+        ),
       ),
     );
   }
